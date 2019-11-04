51be7ba2e20adefdb0c48d8790b143236ca31689
Nahid Hasan <50502837+Nahid2194@users.noreply.github.com>
2019-11-04 09:49:38 +0600

    Add files via upload

--- quick-sort.c
+++ quick-sort.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,arr[10],temp=0,n;
+    int i,j,arr[10],temp,n;
     printf("Enter array size: ");
-     scanf("%d",&n);
-     printf("Enter array Element: ");
-    for(i=0;i<n;i++)
+    scanf("%d",&n);
+    printf("Enter array Element: ");
+    for(i=0; i<n; i++)
     {
-         scanf("%d",&arr[i]);
+        scanf("%d",&arr[i]);
     }
     quick_sort(arr,0,n-1);
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
-         printf("%d ",arr[i]);
+        printf("%d ",arr[i]);
     }
 }
-quick_sort(int arr[],int start,int end)
+int quick_sort(int arr[],int start,int end)
 {
     int pIndex;
     if(start<end)
@@ -25,18 +25,19 @@ quick_sort(int arr[],int start,int end)
         quick_sort(arr,pIndex+1,end);
     }
 }
-partition(int arr[],int start,int end)
+int partition(int arr[],int start,int end)
 {
     int pivot,pIndex,i,temp;
     pivot=arr[end];
     pIndex=start;
-    for(i=start;i<end-1;i++)
+    for(i=start; i<=end-1; i++)
     {
         if(arr[i]<=pivot)
         {
             temp=arr[i];
             arr[i]=arr[pIndex];
             arr[pIndex]=temp;
+            pIndex=pIndex+1;
 
         }
     }
